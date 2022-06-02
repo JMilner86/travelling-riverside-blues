8c2c3ed49037debc2131dc2f29d567c2d5209044
Jonah Milner <jonahmilner86@gmail.com>
2022-06-01 20:14:13 -0600

    adding functionality for retrieving the teporary token

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,24 @@
 // Trading Card App?
 let searchBar = document.querySelector('.search');
 let getButton = document.querySelector('.get');
+// let dotenv = require('dotenv')
+// let apiKey = process.env.API_KEY
 
 
-fetch('https://project.trumedianetworks.com/api/nfl/players')
+
+// fetch('https://project.trumedianetworks.com/api/nfl/players')
+// .then(response => response.json())
+// .then(data => {
+
+// })
+
+
+getButton.addEventListener('click', function(){
+fetch(`https://project.trumedianetworks.com/api/token`,{
+    headers: { apiKey: `${apiKey}`}
+})
 .then(response => response.json())
 .then(data => {
-
+    console.log(data)
+})    
 })
